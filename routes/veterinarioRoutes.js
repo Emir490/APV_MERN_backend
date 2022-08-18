250532fd86a99a9fff862e39b9b9ae4111e52021
Emir490 <chido_emir@hotmail.com>
2022-08-18 00:51:10 -0700

    first commit

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { autenticar, comprobarToken, confirmar, nuevoPassword, olvidePassword, perfil, registrar, actualizarPerfil, actualizarPassword } from "../controllers/veterinarioController.js";
+import checkAuth from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+// área pública
+router.post('/', registrar);
+router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+// Área privada
+router.get('/perfil', checkAuth, perfil);
+router.put('/perfil/:id', checkAuth, actualizarPerfil);
+router.put('/actualizar-password', checkAuth, actualizarPassword);
+
+export default router;
